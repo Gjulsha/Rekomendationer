5759e289860d5e0e14af7a2c5a02644a98cac4e4
Gjulsha <gjulsha.ademi@elev.ga.lbs.se>
2026-04-08 11:06:45 +0200

    Add files via upload

---
+++ Film_Rekomendationer.py
@@ -0,0 +1,13 @@
+
+movies =[
+    {"title":"Spider-Man: No Way Home","genre":"Action","mood":"Exciting","age":"11+"}
+    {"title":"Home Alone","genre":"Comedy","mood":"Nostalgia","age":"11+"}
+    {"title":"Titanic","genre":"Drama","mood":"Tragedy","age":"11+"}
+    {"title":"The Conjuring","genre":"Horror","mood":"Fear","age":"15+"}
+    {"title":"Avatar","genre":"Sci-Fi","mood":"Breathtaking","age":"11+"}
+    {"title":"The Notebook","genre":"Romance","mood":"Love","age":"15+"}
+    {"title":"Shrek","genre":"Shrek","mood":"Fun","age":"0+"}
+    {"title":"Jurassic Park","genre":"Thriller","mood":"Thrill","age":"10+"}
+    {"title":"Harry Potter and the Sorcerer's Stone","genre":"Fantasy","mood":"Wonder","age":"7+"}
+    {"title":"The Godfather","genre":"Crime","mood":"Power","age":"15+"}
+    ]
